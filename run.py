b35fa0c5b9de35909b2f2d78e5aa90f5a649fc66
Goncalves95 <>
2023-11-14 14:50:17 +0000

    Correting the invalid data message for apperar on console

--- run.py
+++ run.py
@@ -15,22 +15,21 @@ SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
 def get_sales_data():
     """
-    Get sales figures imput from the user
+    Get sales figures input from the user.
     """
     print("Please enter sales data from the last market.")
     print("Data should be six numbers, separated by commas.")
     print("Example: 10,20,30,40,50,60\n")
 
     data_str = input("Enter your data here: ")
-    
     sales_data = data_str.split(",")
-    print(sales_data)
+    validate_data(sales_data)
 
 
 def validate_data(values):
     """
     Inside the try, converts all string values into integers.
-    Raises ValueError if trings cannot be converted into int,
+    Raises ValueError if strings cannot be converted into int,
     or if there aren't exactly 6 values.
     """
     try:
